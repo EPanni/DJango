aed26a2ff52959aaee6c90015c55521ee3567877
Erick Panni <erickpanni@icloud.com>
2022-07-13 20:42:28 -0300

    REFACTOR[Admin_Settings]:Modifying what's displayed

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display=('id','name','surname','phone','email','creation_date')
+    list_display_links = ('id','name', 'surname')
+    list_filter = ('name','surname')
+    list_per_page = 20
+    search_fields = ('name','surname')
+
+admin.site.register(Category)
+admin.site.register(Contact,ContactAdmin)
