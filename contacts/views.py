26b3811ee5d976c04c09fb3c23aabe76a892c627
Erick Panni <erickpanni@icloud.com>
2022-07-16 12:04:56 -0300

    REFACTOR[Contacts per page]

--- contacts/views.py
+++ contacts/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contact
 from django.core.paginator import Paginator
-
 def index(request):
-    contacts=Contact.objects.all()
-    paginator = Paginator(contacts,1) # This content was only choosen due to validation purposes
+    contacts=Contact.objects.order_by('name').filter(display=True)
+    paginator = Paginator(contacts,15) # This content was only choosen due to validation purposes
 
     page = request.GET.get('p')
     contacts = paginator.get_page(page)
-
     return render(request,
     'contacts/index.html',{
     'contacts':contacts})
 
 def display_contact(request,contact_id):
     contact=get_object_or_404(Contact,id=contact_id)
+    
+    if not contact.display:
+        raise Http404()
+        
     return render (request,'contacts/display_contact.html',
     {'contact':contact})
 
