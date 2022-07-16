edf2bedc9f3426009a2f443071dab235c1c6fc8d
Erick Panni <erickpanni@icloud.com>
2022-07-16 11:32:01 -0300

    FEAT[Pagination]: Adding pagination

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from .models import Contact
+from django.core.paginator import Paginator
 
 def index(request):
     contacts=Contact.objects.all()
+    paginator = Paginator(contacts,1) # This content was only choosen due to validation purposes
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
+
     return render(request,
     'contacts/index.html',{
     'contacts':contacts})
