94147185ea567c899bf5227d60af654cb75876f0
Erick Panni <erickpanni@icloud.com>
2022-07-21 21:20:37 -0300

    FEAT[Registering users]: Added validation and function to create a new user

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,12 @@
 but it is no the focus on this moment and will be refactored in the future"""
 
 # TODO: REFACTOR DOCSTRINGS FROM THE WHOLE PROJECT
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -17,7 +22,54 @@ def logout(request):
 
 def register(request):
     """register"""
-    return render(request, "account/register.html")
+    if request.method != "POST":
+        return render(request, "account/register.html")
+
+    fields = ["name", "surname", "email", "user", "password", "password2"]
+    fields_values = {}
+
+    for field in fields:
+        fields_values[field] = request.POST.get((field))
+        if not fields_values[field]:
+            messages.error(request, "All values must be filled")
+            return render(request, "account/register.html")
+
+    # TODO: Implement Tests and add more validations
+    print(fields_values["name"])
+    try:
+        validate_email(fields_values["email"])
+
+    except ValidationError:
+        messages.error(request, "The entered emails is not valid,")
+        return render(request, "account/register.html")
+
+    if len(fields_values["password"]) < 6:
+        messages.error(request, "The password has to contain at least 6 digits")
+        return render(request, "account/register.html")
+
+    if User.objects.filter(username=fields_values["user"]).exists():
+        messages.error(request, "The user already exists")
+        return render(request, "account/register.html")
+
+    if User.objects.filter(email=fields_values["email"]).exists():
+        messages.error(request, "The emai has already being used ")
+        return render(request, "account/register.html")
+
+    if fields_values["password"] != fields_values["password2"]:
+        messages.error(request, "The passwords must match")
+        return render(request, "account/register.html")
+
+    messages.success(request, 'Successfully registered"  Please login in you account.')
+    user = User.objects.create_user(
+        username=fields_values["user"],
+        email=fields_values["email"],
+        password=fields_values["password"],
+        first_name=fields_values["name"],
+        last_name=fields_values["surname"],
+    )
+
+    user.save()
+    return redirect("login")
 
 
 def dashboard(request):
